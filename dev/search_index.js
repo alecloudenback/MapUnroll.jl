6616218dc86d3b90ded983601d99268f2b554131
Documenter.jl <documenter@juliadocs.github.io>
2025-07-12 14:41:10 +0000

    build based on 9db903a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#MapUnroll","page":"Home","title":"MapUnroll","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for MapUnroll.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
